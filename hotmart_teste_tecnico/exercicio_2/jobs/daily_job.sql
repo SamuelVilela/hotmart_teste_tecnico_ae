49c9a810d215233520f2845ed3ba19f307d3d99c
Samuel Vilela <samuel.souza.vilela@gmail.com>
2026-02-11 10:26:39 -0300

    Fix: Ajuste Script

--- hotmart_teste_tecnico/exercicio_2/jobs/daily_job.sql
+++ hotmart_teste_tecnico/exercicio_2/jobs/daily_job.sql
@@ -2,7 +2,7 @@
 -- Staging
 RUN sql/staging/01_purchase_state.sql;
 RUN sql/staging/02_product_item_state.sql;
-RUN sql/staging/03_purchase_extra_state.sql;
+RUN sql/staging/03_purchase_extra_info_state.sql;
 -- Snapshot
 RUN sql/snapshot/01_snapshot_d1.sql;
 -- Merge
--- hotmart_teste_tecnico/exercicio_2/sql/staging/03_purchase_extra_info_state.sql
+++ hotmart_teste_tecnico/exercicio_2/sql/staging/03_purchase_extra_info_state.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE TEMP VIEW purchase_extra_state AS
+CREATE OR REPLACE TEMP VIEW purchase_extra_info_state AS
 SELECT
   purchase_id,
   purchase_partition,
   MAX_BY(subsidiary, transaction_datetime) AS subsidiary
 FROM purchase_extra_info
 WHERE transaction_date < CURRENT_DATE()
-GROUP BY purchase_id, purchase_partition
+GROUP BY purchase_id, purchase_partition;
